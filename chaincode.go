14c0cd6c6600c0ff10d26d0ef1b68537f33eed8f
Raval <parth.raval@capgemini.com>
2017-07-12 13:49:39 +0530

    initial changes

--- chaincode.go
+++ chaincode.go
@@ -1,1 +1,54 @@
 
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// Region Chaincode implementation
+type DTC_Chaincode struct {
+}
+
+func (t *DTC_Chaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var err error
+	if len(args) != 0 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 0")
+	}
+
+	//Create database on blockchain
+	err = initializeChaincode(stub, args)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (t *DTC_Chaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	if function == "testPost" {
+		// Initialize the User
+		return testPost(stub, args)
+	}
+	return nil, nil
+}
+
+func (t *DTC_Chaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	if function == "testGet" {
+		// Read contract details from blockchain
+		return testGet(stub, args)
+	} 
+
+	return nil, nil
+}
+
+func main() {
+	err := shim.Start(new(DTC_Chaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
--- logic.go
+++ logic.go
@@ -1,1 +1,77 @@
 
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+    "io/ioutil"
+    "bytes"
+    "net/http"
+    "log"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func testPost(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	var ok bool
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Need 3 arguments")
+	}
+
+	contractId := args[0]
+	attachmentName := args[1]
+	documentBlob := args[2]
+
+	ok, err = insertAttachmentDetails(stub, contractId, attachmentName, documentBlob)
+	if !ok && err == nil {
+		return nil, errors.New("Error in inserting attachment")
+	}
+
+	return nil, err
+}
+
+func testGet(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 0 {
+		return nil, errors.New("Incorrect number of arguments. Need 0 arguments")
+	}
+
+	urlGet := "http://localhost:8080/api/login/test"
+
+    spaceClient := http.Client{
+        Timeout: time.Second * 2, // Maximum of 2 secs
+    }
+
+    req, err := http.NewRequest(http.MethodGet, urlGet, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    //req.Header.Set("User-Agent", "spacecount-tutorial")
+
+    res, getErr := spaceClient.Do(req)
+    if getErr != nil {
+        log.Fatal(getErr)
+    }
+
+    body, readErr := ioutil.ReadAll(res.Body)
+    if readErr != nil {
+        log.Fatal(readErr)
+    }
+
+    person1 := person{}
+    jsonErr := json.Unmarshal(body, &person1)
+    if jsonErr != nil {
+        log.Fatal(jsonErr)
+    }
+
+	fmt.Printf("HTTP: %s\n", res.Status)
+    fmt.Println(person1)
+
+	return person1, nil
+}
